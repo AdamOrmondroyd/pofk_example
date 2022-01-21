d3c574c392f68cce714c1c73ce8d5acbd41d4ade
Ormorod <Adam.Ormondroyd@gmail.com>
2022-01-21 11:46:10 +0000

    Added pofk.py, which is a copy of FIG 7 from the Cobaya paper

---
+++ src/pofk.py
@@ -0,0 +1,87 @@
+import numpy as np
+from cobaya.theory import Theory
+
+
+def feature_envelope(k, c, w):
+    """
+    Returns the value of the envelope at k. The envelope functional form is
+
+        env(k) = exp(-0.5 (log10(k/c)/w)**2) * sin(2πk/l)
+
+    """
+    return np.exp(-((np.log10(k / c) / w) ** 2) / 2)  ## um where's sine gone?
+
+
+def feature_power_spectrum(
+    As, ns, A, l, c, w, kmin=1e-6, kmax=10, k_pivot=0.05, n_samples_wavelength=20
+):
+    """
+    Creates the primordial scalar power spectrum as a power law plus an oscillatory
+    feature of given amplitude A and wavelength l, cantered at c with a lognormal envelope
+    of log10-width w, as
+
+        ΔP/P = A * exp(-0.5 (log10(k/c)/w)**2) * sin(2πk/l)
+
+    Ther characteristic Δk is determined by the number of samples per oscillation
+    n_samples_wavelength (default: 20).
+
+    Returns a sample of k, P(k)
+    """
+    # Ensure thin enough sampling at low-k
+    Δk = min(0.0005, 1 / n_samples_wavelength)
+    ks = np.arange(kmin, kmax, Δk)
+    power_law = lambda k: As * (k / k_pivot) ** (ns - 1)
+    ΔPoverP = lambda k: (A * feature_envelope(k, c, w) * np.sin(2 * np.pi * k / l))
+    Pks = power_law(ks) * (1 + ΔPoverP(ks))
+    return ks, Pks
+
+
+class FeaturePrimordialPk(Theory):
+    """
+    Theory class producing a slow-roll-like power spectrum with an enveloped,
+    lineary-oscillatory feature on top.
+    """
+
+    params = {
+        "As": None,
+        "ns": None,
+        "amplitude": None,
+        "wavelength": None,
+        "centre": None,
+        "logwidth": None,
+    }
+
+    n_samples_wavelength = 20
+    k_pivot = 0.05
+
+    def calculate(self, state, want_derived=True, **params_values_dict):
+        As, ns, amplitude, wavelength, centre, logwidth = [
+            params_values_dict[p] for p in self.params.keys()
+        ]
+        ks, Pks = feature_power_spectrum(
+            As,
+            ns,
+            amplitude,
+            wavelength,
+            centre,
+            logwidth,
+            kmin=1e-6,
+            kmax=10,
+            k_pivot=self.k_pivot,
+            n_samples_wavelength=self.n_samples_wavelength,
+        )
+        state["primordial_scalar_pk"] = {"k": ks, "Pk": Pks, "log_regular": False}
+
+    def get_primordial_scalar_pk(self):
+        return self.current_state["primordial_scalar_pk"]
+
+
+def logprior_high_k(A, c, w, k_high=0.25, A_min=5e-3):
+    """
+    Returns -inf whenever the feaure acts at too high ks only, i.e. s.t. the
+    product of amplitude and envelope at "k_high" is smaller than "A_min", given that the
+    envelope is centred at "k > k_high.
+    """
+    if c < k_high:
+        return 0
+    return 0 if A * feature_envelope(k_high, c, w) > A_min else -np.inf
