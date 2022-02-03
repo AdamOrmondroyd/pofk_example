07dbcaacf79ab4dede64a55e1f2707be58c85d45
Ormorod <Adam.Ormondroyd@gmail.com>
2022-02-03 10:04:23 +0000

    removed commented-out camb_polychord call

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 This file is an alternative to calling something like
 $ python -m cobaya-run -p packages -o runs/pofk -r/-f --test etc
 
-This exists so I can call it in debug mode in the way I know how
+This exists so I can call it in debug mode in the way I know
 """
 
 import cobaya
@@ -19,14 +19,3 @@ cobaya.run(
     test=False,
     debug=False,
 )
-
-## standard camb
-# cobaya.run(
-#     "camb_polychord.yaml",
-#     "packages",
-#     "runs/test",
-#     resume=resume,
-#     force=not resume,
-#     test=True,
-#     debug=False,
-# )
